ba7fb5bf8415c0ef9a7f6d63bb41ef3d4cbe55ef
Marco Vincenzi <marcovincenzi@thenemesis.io>
2026-02-26 17:35:35 +0100

    Fixed Generator SO props not being serialized

--- Runtime/GeneratorData.cs
+++ Runtime/GeneratorData.cs
@@ -7,8 +7,8 @@ namespace Noya.Resgen
     [PublicAPI]
     public abstract class GeneratorData<TResource, TValue> : ScriptableObject where TResource : struct, Enum
     {
-        public GeneratorType GeneratorType { get; protected set; }
-        public TResource Resource { get; protected set; }
-        public TValue Value { get; protected set; }
+        [field:SerializeField] public GeneratorType GeneratorType { get; protected set; }
+        [field:SerializeField] public TResource Resource { get; protected set; }
+        [field:SerializeField] public TValue Value { get; protected set; }
     }
 }
